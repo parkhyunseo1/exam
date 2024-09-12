a130e08d7d3c0805ad32515a29aa3d04a31aed66
박현서 <qkr2625@gmail.com>
2024-09-12 17:41:48 +0900

    게터추가

---
+++ test/src/Account.java
@@ -0,0 +1,11 @@
+public class Account {
+
+    private String onwer;
+    private int balance;
+    private String accountNo;
+
+    public String getAccountNo() {
+        return accountNo;
+    }
+
+}
